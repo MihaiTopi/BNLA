b13eae3f4b6215f26a5419f018511b2070049a6d
MihaiTopi <mihai.topor@outlook.com>
2025-05-27 18:44:04 +0300

    deploy12

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // ✅ Correct: Just define the path separately
 const DIST_FOLDER = path.join(__dirname, 'dist/angular-projet1/browser');
